2835658d73e819dacc4aecae079fc11bd5a0aa3f
yonguelink <isaac.computing@gmail.com>
2015-09-28 08:03:51 -0400

    started adding Angular

--- assets/js/isaacotherrien.js
+++ assets/js/isaacotherrien.js
@@ -1,5 +1,48 @@
 var isaac = new Firebase("https://isaacotherrien.firebaseio.com/");
 
-document.onload = function start(){
-	console.log("I start");
+var portfolio = angular.module("portfolio", ["firebase"]);
+
+portfolio.controller("HeaderCtrl", function($scope, $firebaseObject){
+	var sync = $firebaseObject(isaac.child("Me"));
+	sync.$bindTo($scope, "info");
+});
+
+portfolio.controller("OneCtrl", function($scope, $firebaseObject){
+	var sync = $firebaseObject(isaac.child("About me"));
+	sync.$bindTo($scope, "aboutMe");
+});
+
+portfolio.controller("RecentCtrl", function($scope, $firebaseObject){
+	var sync = $firebaseObject(isaac.child("Recent Work"));
+	sync.$bindTo($scope, "works");
+});
+
+portfolio.controller("InTouchCtrl", function($scope, $firebaseObject){
+	var sync = $firebaseObject(isaac.child("Get in touch"));
+	sync.$bindTo($scope, "touch");
+});
+
+portfolio.controller("FooterCtrl", function($scope, $firebaseObject){
+	var sync = $firebaseObject(isaac.child("footer"));
+	sync.$bindTo($scope, "footer");
+});
+
+function save(){
+	name = $("#userName").val();
+	if(name == ""){
+		alert("You must have a name.");
+		return;
+	}
+	email = $("#userEmail").val();
+	var reg = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	if(!reg.test(email)){
+		alert("You must have a valid email.");
+		return;
+	}
+	message = $("#userMessage").val();
+	if(message.length <= 2){
+		alert("You must enter a significative message.");
+	}
+	userMsg = {"name":name, "email":email, "message":message};
+	isaac.child("messages").push(userMsg);
 }
