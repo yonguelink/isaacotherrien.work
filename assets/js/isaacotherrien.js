9ae40e4b6e602c5a9963dcf74ebc6b18faa08106
yonguelink <isaac.computing@gmail.com>
2015-10-01 20:19:30 -0400

    Content is now 100% customizable, by anyone who has: 1.  User ID in Firebase, 2. Their user ID has the value "editable:true" in the database, 3. They have registered with a user name ending in "@isaacotherrien.work"

--- assets/js/isaacotherrien.js
+++ assets/js/isaacotherrien.js
@@ -1,32 +1,47 @@
 var isaac = new Firebase("https://isaacotherrien.firebaseio.com/");
 
 var portfolio = angular.module("portfolio", ["firebase"]);
-var a;
+var adminAuth = isaac.getAuth();
+var edit = false;
 
 portfolio.controller("HeaderCtrl", function($scope, $firebaseObject){
-	var sync = $firebaseObject(isaac.child("Me"));
-	sync.$bindTo($scope, "info");
+	$scope.info = $firebaseObject(isaac.child("Me"));
+	$scope.editable = editable($firebaseObject);
 });
 
 portfolio.controller("OneCtrl", function($scope, $firebaseObject){
-	var sync = $firebaseObject(isaac.child("About me"));
-	sync.$bindTo($scope, "aboutMe");
+	$scope.aboutMe = $firebaseObject(isaac.child("AboutMe"));
+	$scope.editable = editable($firebaseObject);
 });
 
 portfolio.controller("RecentCtrl", function($scope, $firebaseObject){
-	var sync = $firebaseObject(isaac.child("Recent Work"));
-	sync.$bindTo($scope, "works");
+	$scope.works = $firebaseObject(isaac.child("RecentWork"));
+	$scope.editable = editable($firebaseObject);
 });
 
 portfolio.controller("InTouchCtrl", function($scope, $firebaseObject){
-	var sync = $firebaseObject(isaac.child("Get in touch"));
-	sync.$bindTo($scope, "touch");
+	$scope.touch = $firebaseObject(isaac.child("GetInTouch"));
+	$scope.editable = editable($firebaseObject);
 });
 
 portfolio.controller("FooterCtrl", function($scope, $firebaseObject){
-	var sync = $firebaseObject(isaac.child("footer"));
-	sync.$bindTo($scope, "footer");
+	$scope.footer = $firebaseObject(isaac.child("Footer"));
+	$scope.editable = editable($firebaseObject);
 });
+
+portfolio.controller("SaveBtnCtrl", function($scope, $firebaseObject){
+	$scope.save = editable($firebaseObject);
+});
+
+portfolio.controller("AdminCtrl", function($scope, $firebaseObject){
+	$scope.admin = editable($firebaseObject);
+});
+
+function editable($firebaseObject){
+	if(adminAuth)
+		return $firebaseObject(isaac.child(adminAuth.uid));
+	return $firebaseObject(isaac.child(0));
+}
 
 function save(){
 	name = $("#userName").val();
@@ -46,4 +61,63 @@ function save(){
 	}
 	userMsg = {"name":name, "email":email, "message":message};
 	isaac.child("messages").push(userMsg);
+}
+
+function search(e){
+	e = e || window.event;
+	if (e.keyCode == 13)
+		login();
+}
+
+function saveAllText(){
+	$('#myPage').contents().find("span[contenteditable='true']").each(function(){
+		loc = this.attributes.getNamedItem("location");
+		val = this.innerHTML;
+		tempIsaac = isaac;
+		if(loc){
+			loc = loc.nodeValue.split(" ");
+			for(pos in loc){
+				child = loc[pos];
+				if(!isNaN(child)){
+					if(child < 10)
+						child = "0" + child;
+					else
+						child = child.toString();
+				}
+				if(pos == loc.length-1){
+					toSend = {};
+					toSend[child] = val;
+					tempIsaac.update(toSend);
+				}else
+					tempIsaac = tempIsaac.child(child);
+			}
+		}
+	});
+	redirect("index.html");
+}
+
+function login(){
+	userName = $("#userName").val() + "@isaacotherrien.work";
+	userPassword = $("#userPassword").val();
+	isaac.authWithPassword({
+		email    : userName,
+		password : userPassword
+	},function(error, authData) {
+		if (error) {
+			console.log("Login Failed!", error);
+		}else{
+			redirect("index.html");
+		}
+	});
+}
+
+function logout(){
+	isaac.unauth();
+	redirect("index.html");
+}
+
+function redirect(page){
+	page = page || "admin.html";
+	//Redirect to page page
+	window.location = page;
 }
