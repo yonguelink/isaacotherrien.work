748bc10137fdaf02a7f45835df8352a6a8fdf152
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2015-10-05 20:16:31 -0400

    Separated S3 of git

--- push.bat
+++ push.bat
@@ -2,9 +2,12 @@
 call check.bat
 set /p ask=Upload to S3 (y/n)? 
 if %ask% == y (
+	call doPush.bat
+)
+set /p gitdo=Push to git (y/n)? 
+if %gitdo% == y (
 	git add .
 	set /p msg=Git commit message : 
 	git commit -m "%msg%"
 	git push
-	call doPush.bat
 )
