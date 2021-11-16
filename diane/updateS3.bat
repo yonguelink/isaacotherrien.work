26d2d342e980802a7775f5ac80705a6e2f5dc0bf
Isaac Ouellet-Therrien <isaac.computing@gmail.com>
2021-11-16 18:57:16 -0500

    Remove useless bats

--- diane/updateS3.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-aws s3 sync . s3://dianeouellet.ca --exclude ".git/*" --exclude "LICENSE" --exclude "*.md" --exclude ".git*" --exclude "*.bat"
--- doPush.bat
+++
@@ -1,1 +0,0 @@
-aws s3 sync . s3://isaacotherrien.work --exclude ".git/*" --exclude "LICENSE" --exclude "*.md" --exclude ".git*" --exclude "*.bat"
