35435c5b8bfdb322290ca1064d529f2b9eaff7fe
Isaac Ouellet-Therrien <isaac.computing@gmail.com>
2021-11-16 19:35:38 -0500

    Update mail to get info from body

--- diane/netlify/functions/mail.js
+++ diane/netlify/functions/mail.js
@@ -5,7 +5,17 @@ const fromEmail = process.env.FROM_EMAIL;
 const fromPassword = process.env.FROM_PASSWORD;
 
 exports.handler = async function (event) {
-    console.log(event)
+    const body = JSON.parse(event.body);
+
+    if (body.email == null || body.email.length < 2) {
+        console.error(`An email can't have 3 chars. Received: '${body.email}'`)
+        return;
+    }
+
+    if (body.message == null || body.message.length < 5) {
+        console.error(`A message must have more than 5 chars. Received: '${body.message}'`)
+    }
+
     const transporter = nodemailer.createTransport({
         host: 'smtp.zoho.com',
         port: 465,
@@ -16,13 +26,12 @@ exports.handler = async function (event) {
         }
     })
 
+    const message = `Message de: ${body.name}, avec le courriel: ${body.email}\n\n${message}, en date du: ${body.date}`;
     await transporter.sendMail({
-        from: `"Diane Ouellet" <${fromEmail}>`,
-        to: 'isaac.computing@gmail.com',
-        subject: 'Hello',
-        text: event.body,
-        html: event.body
+        from: `"dianeouellet.ca" <${fromEmail}>`,
+        to: fromEmail,
+        subject: `Message de ${body.name} (${body.email})`,
+        text: message,
+        html: message
     })
-
-    console.log('Sent!')
 }
