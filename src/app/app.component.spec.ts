c8814e384a94cc9053977e792759bcc184362010
yonguelink <isaac.computing@gmail.com>
2024-02-06 08:37:58 -0500

    Prettier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, isaacotherrien.work');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, isaacotherrien.work',
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
 export class AppComponent {
   title = 'isaacotherrien.work';
