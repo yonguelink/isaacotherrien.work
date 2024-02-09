222a4c6af5de4836276468b98810be688299e146
yonguelink <isaac.computing@gmail.com>
2024-02-09 18:53:42 -0500

    Contact form should work

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+  Form,
+  FormsModule,
+  FormGroup,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'contact',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.sass',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  form = new FormGroup({
+    subject: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+    name: new FormControl('', [Validators.required]),
+  });
+
+  sendEmail() {
+    if (this.form.invalid) {
+      return;
+    }
+    window.open(
+      `mailto:isaac@isaacotherrien.work?subject=${this.form.controls.subject.value}&body=From: ${this.form.controls.name.value}\n${this.form.controls.message.value}`,
+    );
+  }
+
+  getErrorMessage(validator: FormControl) {
+    if (validator.errors?.['required']) {
+      return 'You must enter a value';
+    }
+
+    if (validator.errors?.['minlength']) {
+      return `At least ${validator.errors?.['minlength'].requiredLength} characters required`;
+    }
+
+    return validator.invalid ? 'Not a valid email' : '';
+  }
+}
