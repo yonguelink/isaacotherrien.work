b5d3be430217f623133d8bc5569ff5a65df95c28
yonguelink <isaac.computing@gmail.com>
2024-02-09 12:51:24 -0500

    Clean up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { AboutComponent } from '../about/about.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { FooterComponent } from '../footer/footer.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, AboutComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
