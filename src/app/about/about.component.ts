36eb29c4f16b52359388893cb530e2841d5a352b
yonguelink <isaac.computing@gmail.com>
2024-02-09 12:51:29 -0500

    Add all pages

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.sass'
+})
+export class AboutComponent {
+
+}
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.sass'
+})
+export class ContactComponent {
+
+}
