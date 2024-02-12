324b6376fa1981a625ebeee5989e7f62fdcb5252
yonguelink <isaac.computing@gmail.com>
2024-02-12 14:48:36 -0500

    Menu works now

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'menu',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  menuOpen: boolean = false;
+  closeMenu() {
+    this.menuOpen = false;
+  }
+}
