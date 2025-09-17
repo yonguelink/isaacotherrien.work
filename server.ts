fa9674c367ab61a8f0f051b1b544edb428329636
yonguelink <isaac.computing@gmail.com>
2025-09-17 19:18:24 -0400

    Remove netlify stuff

--- server.ts
+++ server.ts
@@ -5,31 +5,6 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 import { existsSync } from 'node:fs';
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-import { getContext } from '@netlify/angular-runtime/context.mjs';
-
-const angularAppEngine = new AngularAppEngine();
-
-export async function netlifyAppEngineHandler(
-  request: Request,
-): Promise<Response> {
-  const context = getContext();
-
-  // Example API endpoints can be defined here.
-  // Uncomment and define endpoints as necessary.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
-
-  const result = await angularAppEngine.handle(request, context);
-  return result || new Response('Not found', { status: 404 });
-}
-
-/**
- * The request handler used by the Angular CLI (dev-server and during build).
- */
-export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
