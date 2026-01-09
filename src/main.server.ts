cde745055bafb903a8ca37ef0cfc1034d5430617
yonguelink <isaac.computing@gmail.com>
2026-01-09 15:36:07 -0500

    Update angular core

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { config } from './app/app.config.server';
 import { PageComponent } from './app/page/page.component';
 
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(PageComponent, config, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(PageComponent, {...config, providers: [provideZoneChangeDetection(), ...config.providers]}, context);
 
 export default bootstrap;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import { provideZoneChangeDetection } from "@angular/core";
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { PageComponent } from './app/page/page.component';
 
-bootstrapApplication(PageComponent, appConfig).catch((err) =>
+bootstrapApplication(PageComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]}).catch((err) =>
   console.error(err),
 );
